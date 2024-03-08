a87b2df4005f56e03e070ea72ab96aa604f1ad28
txeptsyip <lekoro@hotmail.co.uk>
2024-03-08 20:41:58 +0000

    added TMP and more main menu

---
+++ Assets/Scripts/MainMenuScripts/MainMenuScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject OptionsMenu;
+    public GameObject MainMenu;
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("quit button was pressed");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Debug.Log("the new game button was pressed");
+    }
+
+    public void Options()
+    {
+        OptionsMenu.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+}
