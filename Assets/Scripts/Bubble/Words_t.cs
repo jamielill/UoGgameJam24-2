97f73d8634abb70b8dbc8dd1b02c59382926eb5d
oswal99 <118828910+oswal99@users.noreply.github.com>
2024-03-10 10:21:04 +0000

    Bubble folder
    Bubble folder has been add to the main

---
+++ Assets/Scripts/Bubble/Words_t.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Words_t : MonoBehaviour
+{
+    public List<string> A; //name of the list  and the list.
+
+    public TMP_Text textui;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        A = new List<string>(); //the list and its components, 'A.Add("here add the candy or product") 
+        A.Add("Popcorn");
+        A.Add("Candy");
+        A.Add("Drink");
+
+
+        texto_Random();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+  
+
+    public void texto_Random() // Randomness
+    {
+
+        int rand = Random.Range(0,A.Count);
+
+        textui.text = "hello" + A[rand]; //here you can change the text that it shows in the Ui text. A[rand] select one componnent form the list
+
+    } 
+}
