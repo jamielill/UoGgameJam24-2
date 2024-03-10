507c94b9951ac88c0ceca53ce8a903b389cac65c
txeptsyip <lekoro@hotmail.co.uk>
2024-03-10 16:19:06 +0000

    added score textbox

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,6 +9,8 @@ public class GameManager : MonoBehaviour
  public NPCBehavior npc; // Reference to the NPC script
     public Scoring scoringSystem; // Reference to the Scoring system script
     [SerializeField] ScaleScript scale;
+
+    public TMP_Text text;
     
     private int targetScore = 250; // Example target score
     
@@ -19,6 +22,7 @@ if (Input.GetKeyDown(KeyCode.W))
     npc.AssignRandomValue(1, 100); // Example range
     npc.UpdateText(npc.randomValue.ToString());
     Debug.Log("Yo the target score is " + npc.randomValue);
+            text.text = " ";
 }
 
         
@@ -27,6 +31,7 @@ if (Input.GetKeyDown(KeyCode.W))
             // Calculate and log the score based on the NPC's random value
             int score = scoringSystem.CalculateScore(scale.GetList().Count, npc.randomValue);
             Debug.Log("u gave me " + scale.GetList().Count + "But I asked for " + npc.randomValue + "...Keep the change fool... " + score);
+            text.text = ("u gave me " + scale.GetList().Count + "But I asked for " + npc.randomValue + "...Keep the change fool... " + score);
             scale.ResetScales();
         }
     }
