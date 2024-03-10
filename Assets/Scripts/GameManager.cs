3d338208543baa584144ea16146c573ed83ae951
ElieGre <ek9385s@gre.ac.uk>
2024-03-10 13:06:49 +0000

    scores are understood by NPCs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
  public NPCBehavior npc; // Reference to the NPC script
     public Scoring scoringSystem; // Reference to the Scoring system script
-    public ScaleScript scale;
+    [SerializeField] ScaleScript scale;
     
     private int targetScore = 250; // Example target score
     
@@ -22,10 +23,11 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             // Calculate and log the score based on the NPC's random value
-            int score = scoringSystem.CalculateScore(npc.randomValue, targetScore);
+            int score = scoringSystem.CalculateScore(scale.GetList().Count, npc.randomValue);
             Debug.Log($"Calculated Score: {score}");
             scale.ResetScales();
         }
+        Debug.Log(scale.GetList().Count);
     }
 }
 
